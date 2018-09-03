16b11cc4980db36157d922c04e053281deb78d93
Jeffrey Parrish <jeffs.linux@gmail.com>
2018-09-03 10:33:24 -0400

    progress commit, toying with architecture ideas

--- express.js
+++ express.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const axios = require("axios");
 
 app.get('/',(req,res) => {
 	res.send('Version 0');
 });
+
+app.get('/playlist/:id', (req,res) => {
+	res.send(getPlaylist(req));
+});
 app.listen(3000, () => console.log("listening on 3000"));
+
+function getPlaylist(request){
+	var querystring = "?";
+	querysti
+	return axios.get('https://www.googleapis.com/youtube/v3/playlistItems/list'); 
+}
